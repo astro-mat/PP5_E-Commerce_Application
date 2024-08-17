ccfaab9d3e750fed2cea0d47ad8bc192b6d97c77
Matthew Warren <whoismatwarren@gmail.com>
2024-08-17 12:06:22 +0000

    Customized Admin

--- products/models.py
+++ products/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
