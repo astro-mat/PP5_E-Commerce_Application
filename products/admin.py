61b223d7705d9483343f3960610f873976482b78
Matthew Warren <whoismatwarren@gmail.com>
2024-08-19 16:15:56 +0000

    Add new maker model and json file. Add more products

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Maker
 
 # Register your models here.
 
@@ -21,5 +21,12 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+class MakerAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Maker, MakerAdmin)
--- products/models.py
+++ products/models.py
@@ -18,6 +18,19 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+class Maker(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'makers'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
