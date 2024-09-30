0dd673735513998430cf95559f7b7305ce47dfec
Matthew Warren <whoismatwarren@gmail.com>
2024-09-30 11:38:28 +0000

    Add confirmation emails functionality

--- irish_craft_house/urls.py
+++ irish_craft_house/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado URL Configuration
+"""irish craft house URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
