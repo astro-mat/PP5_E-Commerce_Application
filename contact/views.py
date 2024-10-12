c12c93ad7a38dd3293be8beb786e4fe314b476c0
Matthew Warren <whoismatwarren@gmail.com>
2024-10-12 14:51:27 +0000

    Changed email settings

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Contact
 from django.core.mail import send_mail
+from django.conf import settings
 
 def contact_view(request):
     if request.method == 'POST':
