648263c0bdbc6ee2f83eb20df462ff4569b3f11d
Matthew Warren <whoismatwarren@gmail.com>
2024-10-13 15:32:41 +0000

    Changes to wishlist javascript

--- wishlist/views.py
+++ wishlist/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from .models import Wishlist
 from .forms import WishlistForm
 
+from django.http import JsonResponse
+
 from products.models import Product
 
 # Create your views here.
@@ -13,16 +15,21 @@ def add_to_wishlist(request, product_id):
     wishlist, created = Wishlist.objects.get_or_create(user=request.user, item=product)
     if created:
         messages.success(request, "Product added to wishlist")
+        return JsonResponse(data={'created': 'ok'})
     else:
         messages.warning(request, "Product already in wishlist")
-    return redirect("product_detail", product_id=product_id)
+    # return redirect("product_detail", product_id=product_id)
+        return JsonResponse(data={'exists': 'ok'})
 
 @login_required
 def remove_from_wishlist(request, wishlist_id):
     wishlist = get_object_or_404(Wishlist, pk=wishlist_id)
     wishlist.delete()
     messages.success(request, "Product removed from wishlist")
-    return redirect("view_wishlist")
+    # return redirect("view_wishlist")
+    # return JsonResponse(updated_data)
+    return JsonResponse(data={'deleted': 'ok'})
+
 
 @login_required
 def view_wishlist(request):
