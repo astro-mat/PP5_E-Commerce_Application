870b4847145862b64c00b8b887cd6e0c078b9b50
Matthew Warren <whoismatwarren@gmail.com>
2024-10-20 16:13:45 +0000

    Change to wishlist for validation purposes

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,8 +6,8 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = €('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = €('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -36,9 +36,9 @@ card.addEventListener('change', function (event) {
             <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
             </span>
-            <span>${event.error.message}</span>
+            <span>€{event.error.message}</span>
         `;
-        $(errorDiv).html(html);
+        €(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
@@ -50,13 +50,13 @@ var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
+    €('#submit-button').attr('disabled', true);
+    €('#payment-form').fadeToggle(100);
+    €('#loading-overlay').fadeToggle(100);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var saveInfo = Boolean(€('#id-save-info').attr('checked'));
     // From using {% csrf_token %} in the form
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var csrfToken = €('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
@@ -64,33 +64,33 @@ form.addEventListener('submit', function(ev) {
     };
     var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function () {
+    €.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
                 billing_details: {
-                    name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
+                    name: €.trim(form.full_name.value),
+                    phone: €.trim(form.phone_number.value),
+                    email: €.trim(form.email.value),
                     address:{
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
+                        line1: €.trim(form.street_address1.value),
+                        line2: €.trim(form.street_address2.value),
+                        city: €.trim(form.town_or_city.value),
+                        country: €.trim(form.country.value),
+                        state: €.trim(form.county.value),
                     }
                 }
             },
             shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
+                name: €.trim(form.full_name.value),
+                phone: €.trim(form.phone_number.value),
                 address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
+                    line1: €.trim(form.street_address1.value),
+                    line2: €.trim(form.street_address2.value),
+                    city: €.trim(form.town_or_city.value),
+                    country: €.trim(form.country.value),
+                    postal_code: €.trim(form.postcode.value),
+                    state: €.trim(form.county.value),
                 }
             },
         }).then(function(result) {
@@ -100,12 +100,12 @@ form.addEventListener('submit', function(ev) {
                     <span class="icon" role="alert">
                     <i class="fas fa-times"></i>
                     </span>
-                    <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
+                    <span>€{result.error.message}</span>`;
+                €(errorDiv).html(html);
+                €('#payment-form').fadeToggle(100);
+                €('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
-                $('#submit-button').attr('disabled', false);
+                €('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
                     form.submit();
